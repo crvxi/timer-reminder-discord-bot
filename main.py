3ba4e3b3352426b786b98a2f63d6f447393326e7
Jacopo Scatizzi <jacoposcatizzi13@gmail.com>
2025-08-28 15:49:42 +0200

    Add Discord bot functionality with timer command

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+import os
+import discord
+from dotenv import load_dotenv
+from discord import app_commands
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Initialize Discord Bot
+load_dotenv()
+TOKEN: str = os.getenv('DISCORD_TOKEN')
+SERVER_ID: int = int(os.getenv('SERVER_ID'))
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@tree.command(
+    name="timer",
+    description="Set a timer",
+    guild=discord.Object(id=SERVER_ID)
+)
+@app_commands.describe(minutes="Number of minutes for the timer",
+                       message="Reminder message of the timer")
+async def command_timer(interaction, minutes: int, message: str):
+    await interaction.response.send_message(f"Timer set for {minutes} minutes: {message}")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@client.event
+async def on_ready():
+    await tree.sync(guild=discord.Object(id=SERVER_ID))
+    print("Ready!")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+client.run(TOKEN)
